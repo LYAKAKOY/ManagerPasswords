9c5e46a2e53607abd4b7018fd8f555b83d24a6ac
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-24 13:30:42 +0300

    create test_update_user_password

---
+++ backend/tests/test_user_dal/test_update_user.py
@@ -0,0 +1,28 @@
+from db.users.user_dal import UserDAL
+import pytest
+
+
+@pytest.mark.parametrize(
+    "login, password, new_password",
+    [
+        (
+           "login1",
+           "password1",
+           "new_password1"
+        ),
+    ],
+)
+async def test_update_user_password(
+    async_session_test,
+    login,
+    password,
+    new_password
+):
+    session = async_session_test()
+    async with session.begin():
+        user_dal = UserDAL(session)
+        user = await user_dal.create_user(login=login, password=password)
+    async with session.begin():
+        user_dal = UserDAL(session)
+        user = await user_dal.set_password(login=login, password=new_password)
+        assert user.password == new_password
