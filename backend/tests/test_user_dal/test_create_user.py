e9533af4708051e455ca999cff3cf46c2a450b00
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-24 13:09:55 +0300

    create test_create_user_duplicate_login

--- backend/tests/test_user_dal/test_create_user.py
+++ backend/tests/test_user_dal/test_create_user.py
@@ -20,12 +20,21 @@ from tests.db_test import async_session
     ],
 )
 async def test_create_user(
+    async_session_test,
     login,
     password
 ):
-    session = async_session()
+    session = async_session_test()
     async with session.begin():
         user_dal = UserDAL(session)
         user = await user_dal.create_user(login=login, password=password)
         assert user.login == login
         assert user.password == password
+
+async def test_create_user_duplicate_login(async_session_test):
+    session = async_session_test()
+    for _ in range(2):
+        async with session.begin():
+            user_dal = UserDAL(session)
+            user = await user_dal.create_user(login="login", password="12345")
+    assert user is None
