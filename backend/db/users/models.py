7ad3715c407d5522cbde229caadfcd0fca751e17
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-24 10:12:43 +0300

    mapper => mapped

--- backend/db/users/models.py
+++ backend/db/users/models.py
@@ -1,13 +1,13 @@
 import uuid
 
 from sqlalchemy import String
-from sqlalchemy.orm import Mapper, mapped_column
+from sqlalchemy.orm import mapped_column, Mapped
 
 from db.base import Base
 
 
 class User(Base):
 
-    user_id: Mapper[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4(), server_default=str(uuid.uuid4()))
-    login: Mapper[str] = mapped_column(String(15), unique=True, index=True)
-    password: Mapper[str]
+    user_id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4(), server_default=str(uuid.uuid4()))
+    login: Mapped[str] = mapped_column(String(15), unique=True, index=True)
+    password: Mapped[str]
