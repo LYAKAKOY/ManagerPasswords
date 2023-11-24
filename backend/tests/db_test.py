3534513800d0fcc4594f26daed2ca094d26bf272
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-24 13:35:24 +0300

    create test_get_user_by(user_id/login)

--- backend/tests/db_test.py
+++ backend/tests/db_test.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 import settings
 
-engine = create_async_engine(settings.DATABASE_URL, future=True, echo=True)
+engine = create_async_engine(settings.DATABASE_URL, future=True, echo=False)
 async_session = sessionmaker(
         engine,
         class_=AsyncSession,
---
+++ backend/tests/test_user_dal/test_get_user.py
@@ -0,0 +1,49 @@
+from db.users.user_dal import UserDAL
+import pytest
+
+
+@pytest.mark.parametrize(
+    "login, password",
+    [
+        (
+           "login1",
+           "password1"
+        ),
+    ],
+)
+async def test_get_user_by_user_id(
+    async_session_test,
+    login,
+    password
+):
+    session = async_session_test()
+    async with session.begin():
+        user_dal = UserDAL(session)
+        user = await user_dal.create_user(login=login, password=password)
+    async with session.begin():
+        user_dal = UserDAL(session)
+        got_user = await user_dal.get_user_by_user_id(user_id=user.user_id)
+    assert user == got_user
+
+@pytest.mark.parametrize(
+    "login, password",
+    [
+        (
+           "login1",
+           "password1"
+        ),
+    ],
+)
+async def test_get_user_by_login(
+    async_session_test,
+    login,
+    password
+):
+    session = async_session_test()
+    async with session.begin():
+        user_dal = UserDAL(session)
+        user = await user_dal.create_user(login=login, password=password)
+    async with session.begin():
+        user_dal = UserDAL(session)
+        got_user = await user_dal.get_user_by_login(login=user.login)
+    assert user == got_user
