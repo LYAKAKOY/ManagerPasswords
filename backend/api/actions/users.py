7a600165823bb0b691ea1c82e26b5fd1fce1b430
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-25 12:00:36 +0300

    add change password user action

--- backend/api/actions/users.py
+++ backend/api/actions/users.py
@@ -1,6 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from api.users.schemas import CreateUser, ShowUser
+from api.users.schemas import CreateUser, ShowUser, ChangePasswordUser
 from db.users.user_dal import UserDAL
 from hashing import Hasher
 
@@ -9,5 +8,12 @@ async def _create_user(body: CreateUser, session: AsyncSession) -> ShowUser | No
     async with session.begin():
         user_dal = UserDAL(db_session=session)
         user = await user_dal.create_user(login=body.login, password=Hasher.get_password_hash(body.password))
+        if user is not None:
+            return ShowUser(user_id=user.user_id)
+
+async def _change_password_user(body: ChangePasswordUser, session: AsyncSession) -> ShowUser | None:
+    async with session.begin():
+        user_dal = UserDAL(db_session=session)
+        user = await user_dal.set_password(login=body.login, password=Hasher.get_password_hash(body.password))
         if user is not None:
             return ShowUser(user_id=user.user_id)
