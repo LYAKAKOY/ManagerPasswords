bec8d9e00c26210a5efe4c2c76c5377ae55669f3
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-25 12:55:18 +0300

    add user router to main

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
 
+from api.users.handlers import user_router
+
 app = FastAPI(title="ManagerPassword")
 
 main_api_router = APIRouter()
-
+main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(main_api_router)
