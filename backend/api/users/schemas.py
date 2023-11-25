8d55f71dee20947f86423f7f958c6c3b839d29da
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-25 13:12:34 +0300

    add class Check Password

--- backend/api/users/schemas.py
+++ backend/api/users/schemas.py
@@ -1,14 +1,22 @@
 import uuid
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, field_validator
+from password_strength import PasswordStats
 from api.base_schemas import TunedModel
 
-class CreateUser(BaseModel):
+class CheckPasswordStrength:
+    @field_validator("password")
+    def validate_surname(cls, value):
+        if not PasswordStats(value).strength() > 0.5:
+            raise HTTPException(status_code=422, detail="password is too easy")
+        return value
+class CreateUser(CheckPasswordStrength, BaseModel):
     login: str
     password: str
 
 class ShowUser(TunedModel):
     user_id: uuid.UUID
 
-class ChangePasswordUser(BaseModel):
+class ChangePasswordUser(CheckPasswordStrength, BaseModel):
     login: str
     password: str
