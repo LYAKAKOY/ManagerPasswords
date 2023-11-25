32d44f4f871ba292fef048822d2af0b2a1b30f71
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-25 11:53:59 +0300

    add hashing password during create user

--- backend/api/actions/users.py
+++ backend/api/actions/users.py
@@ -2,11 +2,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.users.schemas import CreateUser, ShowUser
 from db.users.user_dal import UserDAL
+from hashing import Hasher
 
 
 async def _create_user(body: CreateUser, session: AsyncSession) -> ShowUser | None:
     async with session.begin():
         user_dal = UserDAL(db_session=session)
-        user = await user_dal.create_user(login=body.login, password=body.password)
+        user = await user_dal.create_user(login=body.login, password=Hasher.get_password_hash(body.password))
         if user is not None:
             return ShowUser(user_id=user.user_id)
