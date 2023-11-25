2f043b63e0b5bb71f5075d4e2183fdf47f551b18
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-25 11:43:42 +0300

    add _create_user action

---
+++ backend/api/actions/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api.users.schemas import CreateUser, ShowUser
+from db.users.user_dal import UserDAL
+
+
+async def _create_user(body: CreateUser, session: AsyncSession) -> ShowUser | None:
+    async with session.begin():
+        user_dal = UserDAL(db_session=session)
+        user = await user_dal.create_user(login=body.login, password=body.password)
+        if user is not None:
+            return ShowUser(user_id=user.user_id)
