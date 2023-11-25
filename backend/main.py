88a5223cb518b22930493804488f983fedb834d4
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-25 12:55:53 +0300

    change title project

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ from fastapi.routing import APIRouter
 
 from api.users.handlers import user_router
 
-app = FastAPI(title="ManagerPassword")
+app = FastAPI(title="ManagerPasswords")
 
 main_api_router = APIRouter()
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
