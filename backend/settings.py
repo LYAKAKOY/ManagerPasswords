d158ad5f078ef4c47cafedb7a8d9ed82f8212296
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-26 23:45:24 +0300

    add ci file

--- backend/settings.py
+++ backend/settings.py
@@ -6,7 +6,7 @@ env = Env()
 DATABASE_URL = env.str(
     "DATABASE_URL",
     default=f"postgresql+asyncpg://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@"
-    f"localhost:5432/{os.environ.get('POSTGRES_DB')}",
+    f"{os.environ.get('DATABASE')}:5432/{os.environ.get('POSTGRES_DB')}",
 )
 
 SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
