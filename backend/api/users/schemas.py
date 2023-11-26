7937175a51c00b0d086d88a786676c6758277876
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-26 12:20:04 +0300

    update class for check password => CheckCredentials(login and password)

--- backend/api/users/schemas.py
+++ backend/api/users/schemas.py
@@ -4,19 +4,24 @@ from pydantic import BaseModel, field_validator
 from password_strength import PasswordStats
 from api.base_schemas import TunedModel
 
-class CheckPasswordStrength:
+class CheckCredentials:
+    @field_validator("login")
+    def validate_surname(cls, value):
+        if not len(value) >= 5:
+            raise HTTPException(status_code=422, detail="login is too short")
+        return value
     @field_validator("password")
     def validate_surname(cls, value):
         if not PasswordStats(value).strength() > 0.5:
             raise HTTPException(status_code=422, detail="password is too simple")
         return value
-class CreateUser(CheckPasswordStrength, BaseModel):
+class CreateUser(CheckCredentials, BaseModel):
     login: str
     password: str
 
 class ShowUser(TunedModel):
     user_id: uuid.UUID
 
-class ChangePasswordUser(CheckPasswordStrength, BaseModel):
+class ChangePasswordUser(CheckCredentials, BaseModel):
     login: str
     password: str
