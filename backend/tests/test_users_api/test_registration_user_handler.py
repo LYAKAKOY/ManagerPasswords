c440004d07f27647a0e915e9d84eb7283e720128
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-26 12:16:41 +0300

    add test_create_user_with_simple_password

--- backend/tests/test_users_api/test_registration_user_handler.py
+++ backend/tests/test_users_api/test_registration_user_handler.py
@@ -31,3 +31,41 @@ async def test_create_user_handler(
     data_from_response = response.json()
     assert response.status_code == expected_status_code
     assert data_from_response.get('user_id') is not None
+
+@pytest.mark.parametrize(
+    "user_data, expected_status_code, expected_data",
+    [
+        (
+                {
+                    "login": "login1",
+                    "password": "12345",
+                },
+                422,
+                {
+                    "detail": "password is too easy"
+                },
+        ),
+        (
+                {
+                    "login": "login2",
+                    "password": "qwerty",
+                },
+                422,
+                {
+                    "detail": "password is too easy"
+                },
+        ),
+    ]
+)
+async def test_create_user_with_simple_password(
+        client,
+        user_data,
+        expected_status_code,
+        expected_data,
+):
+    response = await client.post("/user/reg",
+                                 data=json.dumps(user_data),
+                                 )
+    data_from_response = response.json()
+    assert response.status_code == expected_status_code
+    assert data_from_response == expected_data
