1a8d1f597242ee2be1fcff651b0260b1cabf8a24
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-27 13:47:27 +0300

    add func set password

--- backend/db/passwords/password_dal.py
+++ backend/db/passwords/password_dal.py
@@ -1,3 +1,6 @@
+import uuid
+
+from sqlalchemy import update
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -8,13 +11,24 @@ class PasswordDAL:
     def __init__(self, db_session: AsyncSession):
         self.db_session = db_session
 
-    async def create_password(self, user_id, service_name: str, password: HexByteString) -> Password | None:
+    async def create_password(self, user_id: uuid.UUID, service_name: str, password: HexByteString) -> Password | None:
         new_password = Password(user_id=user_id, service_name=service_name, password=password)
         try:
             self.db_session.add(new_password)
             await self.db_session.flush()
             await self.db_session.commit()
             return new_password
+        except IntegrityError:
+            await self.db_session.rollback()
+            return
+
+    async def set_password(self, user_id: uuid.UUID, service_name: str, password: HexByteString) -> Password | None:
+        query = update(Password).where(Password.user_id == user_id, Password.service_name == service_name).\
+            values(password=password).returning(Password)
+        try:
+            password = await self.db_session.scalar(query)
+            if password is not None:
+                return password
         except IntegrityError:
             await self.db_session.rollback()
             return
