51b9362d10cd53c4ad867c5ecf27d342d40d0f9b
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-28 13:26:33 +0300

    add assert user id

--- backend/tests/test_password_dal/test_get_password.py
+++ backend/tests/test_password_dal/test_get_password.py
@@ -8,27 +8,25 @@ import pytest
     [
         (
            "yandex.ru",
-           "password1"
-
+           "password1",
         ),
         (
            "google.com",
-           "password2"
-
+           "password2",
         ),
     ],
 )
-async def test_create_password(
+async def test_get_password(
     async_session_test,
-    create_user,
+    create_service_password,
     service_name,
-    password
+    password,
 ):
+    user_id = await create_service_password(service=service_name, password=password)
     session = async_session_test()
     async with session.begin():
         password_dal = PasswordDAL(session)
-        service_password = await password_dal.create_password(user_id=create_user, service_name=service_name,
-                                                      password=AES.encrypt_password(password))
-        assert service_password.user_id == create_user
+        service_password = await password_dal.get_password(user_id=user_id, service_name=service_name)
+        assert service_password.user_id == user_id
         assert service_password.service_name == service_name
         assert AES.decrypt_password(service_password.password) == password
