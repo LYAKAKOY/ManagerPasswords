838baf76ab7bf7509b06912acae7ab60a6091275
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-28 11:36:53 +0300

    change type for user_id

--- backend/db/passwords/models.py
+++ backend/db/passwords/models.py
@@ -1,12 +1,12 @@
 from __future__ import annotations
 
+import uuid
 from abc import ABC
 from typing import TYPE_CHECKING
 
 from db.base import Base
 from sqlalchemy import ForeignKey
 from sqlalchemy import String
-from sqlalchemy import UUID
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
@@ -35,7 +35,7 @@ class Password(Base):
     id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
     service_name: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str] = mapped_column(HexByteString)
-    user_id: Mapped[UUID] = mapped_column(
+    user_id: Mapped[uuid.UUID] = mapped_column(
         ForeignKey("users.user_id", ondelete="CASCADE")
     )
 
