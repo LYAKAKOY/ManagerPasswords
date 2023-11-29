2a636581a68e6926ca05e425e2c355757a5f6045
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-30 01:10:39 +0300

    created schemas CreatePassword and ShowPassword

---
+++ backend/api/passwords/schemas.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel
+
+from api.base_schemas import TunedModel
+
+
+class CreatePassword(BaseModel):
+    password: str
+
+
+class ShowPassword(TunedModel):
+    service_name: str
+    password: str
