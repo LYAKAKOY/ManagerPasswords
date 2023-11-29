903bebafe30e443ba87270efc4c4c01b9e0b9eba
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-30 00:59:39 +0300

    refactoring test get password

--- backend/tests/test_password_dal/test_get_password.py
+++ backend/tests/test_password_dal/test_get_password.py
@@ -34,9 +34,9 @@ async def test_get_password_by_service_name(
         assert AES.decrypt_password(service_password.password) == password
 
 @pytest.mark.parametrize(
-    "password_data1, password_data2",
+    "passwords_data",
     [
-        (
+        [
             {
                 "service_name": "yandex.ru",
                 "password": "password1",
@@ -45,26 +45,37 @@ async def test_get_password_by_service_name(
                 "service_name": "google.com",
                 "password": "password2",
             }
-        )
+        ],
+        [
+            {
+                "service_name": "yandex.ru",
+                "password": "password1",
+            },
+            {
+                "service_name": "mail.ru",
+                "password": "password2",
+            },
+            {
+                "service_name": "google.com",
+                "password": "password3",
+            },
+        ],
     ],
 )
 async def test_get_all_passwords(
     async_session_test,
     create_service_password: Callable,
-    password_data1,
-    password_data2,
+    passwords_data,
 ):
-
-    await create_service_password(service=password_data1['service_name'],
-                                            password=password_data1['password'])
-    user_id = await create_service_password(service=password_data2['service_name'],
-                                            password=password_data2['password'])
+    for password_data in passwords_data:
+        user_id = await create_service_password(service=password_data['service_name'],
+                                      password=password_data['password'])
     session = async_session_test()
     async with session.begin():
         password_dal = PasswordDAL(session)
         service_password = await password_dal.get_all_passwords(user_id=user_id)
-        assert len(list(service_password)) == 2
-        for db_service_password, parameter_service in zip(service_password, [password_data1, password_data2]):
+        assert len(list(service_password)) == len(passwords_data)
+        for db_service_password, parameter_service in zip(service_password, passwords_data):
             assert db_service_password.service_name == parameter_service['service_name']
             assert AES.decrypt_password(db_service_password.password) == parameter_service['password']
 
