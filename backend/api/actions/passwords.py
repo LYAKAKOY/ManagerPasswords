8c8fa574b98ba54ff941a02fe1d72f2d1cec4e02
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-30 01:26:01 +0300

    add action get all passwords

--- backend/api/actions/passwords.py
+++ backend/api/actions/passwords.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from crypting import AES
 from api.passwords.schemas import CreatePassword
 from api.passwords.schemas import ShowPassword
@@ -42,3 +44,22 @@ async def _get_password_by_service_name(
                 service_name=service_password.service_name,
                 password=AES.decrypt_password(service_password.password),
             )
+
+async def _get_passwords_by_match_service_name(
+    service_name: str, current_user: User, session: AsyncSession
+) -> List[ShowPassword] | None:
+    async with session.begin():
+        password_dal = PasswordDAL(session)
+        passwords = await password_dal.get_passwords_by_match_service_name(
+            user_id=current_user.user_id, service_name=service_name
+        )
+        if passwords is not None:
+            all_show_passwords = []
+            for password in passwords:
+                all_show_passwords.append(
+                    ShowPassword(
+                        service_name=password.service_name,
+                        password=AES.decrypt_password(password.password),
+                    )
+                )
+            return all_show_passwords
