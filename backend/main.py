65f30c7afb82d63d87b666af15e07134e238fcd8
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-11-30 13:24:21 +0300

    add allow origins and methods

--- backend/main.py
+++ backend/main.py
@@ -4,8 +4,17 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.auth.handlers import auth_router
 from api.users.handlers import user_router
 from api.passwords.handlers import manager_passwords_router
+import settings
 
 app = FastAPI(title="ManagerPasswords")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.ALLOW_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 main_api_router = APIRouter()
 main_api_router.include_router(user_router, prefix="/user", tags=["user"])
