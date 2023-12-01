7306d0085f16c2a7ad360b38a208448e836d7801
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-02 00:00:37 +0300

    user env to get port of database

--- backend/settings.py
+++ backend/settings.py
@@ -8,7 +8,7 @@ env = Env()
 DATABASE_URL = env.str(
     "DATABASE_URL",
     default=f"postgresql+asyncpg://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@"
-    f"{os.environ.get('DATABASE')}:5432/{os.environ.get('POSTGRES_DB')}",
+    f"{os.environ.get('DATABASE')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}",
 )
 
 SECRET_KEY: str = env.str("SECRET_KEY", default="secret_key")
