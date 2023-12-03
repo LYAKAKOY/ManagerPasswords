218fc0d829759fcc9996bc948463974910a7e4c5
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-03 13:39:26 +0300

    add action change master password

--- backend/api/actions/users.py
+++ backend/api/actions/users.py
@@ -1,6 +1,7 @@
 from api.users.schemas import ChangePasswordUser
 from api.users.schemas import CreateUser
 from api.users.schemas import ShowUser
+from db.users.models import User
 from db.users.user_dal import UserDAL
 from hashing import Hasher
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -23,6 +24,18 @@ async def _change_password_user(
         user_dal = UserDAL(db_session=session)
         user = await user_dal.set_password(
             login=body.login, password=Hasher.get_password_hash(body.password)
+        )
+        if user is not None:
+            return ShowUser(user_id=user.user_id)
+
+
+async def _change_master_password_user(
+    user: User, session: AsyncSession
+) -> ShowUser | None:
+    async with session.begin():
+        user_dal = UserDAL(db_session=session)
+        user = await user_dal.generate_new_master_password(
+            user_id=user.user_id, old_aes_key=user.master_password
         )
         if user is not None:
             return ShowUser(user_id=user.user_id)
