54f74143df4430691f566f5b56ad2e3de5ec9f58
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-07 12:38:37 +0300

    delete raise so auth user check valid username and password

--- backend/api/auth/handlers.py
+++ backend/api/auth/handlers.py
@@ -7,8 +7,6 @@ from db.session import AsyncSession
 from db.session import get_db
 from fastapi import APIRouter
 from fastapi import Depends
-from fastapi import HTTPException
-from fastapi import status
 from fastapi.security import OAuth2PasswordRequestForm
 from JWT import create_access_token
 
@@ -20,11 +18,6 @@ async def login_for_access_token(
     form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
 ):
     user = await authenticate_user(form_data.username, form_data.password, db)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-        )
     access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
         data={"sub": str(user.user_id), "other_custom_data": []},
