45e92308db18a54d0541012970cd22295989a1bb
LyaKakoyPC <LYAKAKOYXY@yandex.ru>
2023-12-07 12:39:03 +0300

    add iat to jwt

--- backend/JWT.py
+++ backend/JWT.py
@@ -7,13 +7,12 @@ from jose import jwt
 
 def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
     to_encode = data.copy()
+    time_now = datetime.utcnow()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta
+        expire = time_now + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode.update({"exp": expire})
+        expire = time_now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire, "iat": time_now})
     encoded_jwt = jwt.encode(
         to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
     )
